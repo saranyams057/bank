fccac3fd36db0e2e0e9690deed32a16dbf95b3dc
saranyams057 <61340715+saranyams057@users.noreply.github.com>
2023-03-16 23:55:17 +0530

    Delete bankproject directory

--- bankproject/bankapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Loan
-
-
-class LoanForm(forms.ModelForm):
-    class Meta:
-        model = Loan
-        fields = ['firstname', 'lastname', 'email', 'mobile','gender','dob','address','districts','branches','qualification','specialization']
